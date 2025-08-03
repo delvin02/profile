4b424af8ef704fede023b7af279c2a264590945c
Delvin <delvin.khor@splose.com>
2025-08-03 22:45:10 +0930

    revert legacy build

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		devtoolsJson(),
-		legacy({
-			targets: ['defaults', 'iOS >= 14', 'Safari >= 14'],
-			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-			modernPolyfills: true
-		})
-	]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	build: {
+		target: 'es2022'
+	}
 });
