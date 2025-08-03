e45f297ffd61b538694f4a15fcc9f5bd41422dca
Delvin <delvin.khor@splose.com>
2025-08-03 23:54:06 +0930

    revert changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		devtoolsJson(),
-		legacy({
-			targets: 'iOS >= 12',
-			polyfills: true
-		})
-	]
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });
